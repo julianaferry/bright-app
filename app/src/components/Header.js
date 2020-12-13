9969ea7e96e02d88d5da2891f717fb75190430ae
Juliana <julianafery@gmail.com>
2020-12-13 13:12:20 +0000

    added first files

---
+++ app/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-Components';
+
+export default function Header () {
+
+    return (
+      <>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="/#" >
+                <img src="../logo/Bright-HR-Logo-01 (01).png" alt="brighhr-logo" />
+                </a>
+            </div>
+        </nav>  
+      </>
+    );
+  };
+  
+  
---
+++ app/src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('get document list', () => {
+  // render(<DocumentList />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
